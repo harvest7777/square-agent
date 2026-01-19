7fa92d6c61acdd36ad868f3b341b53f54ace526a
harvest7777 <333harvest.moon333@gmail.com>
2026-01-19 11:57:40 -0800

    chor: add todos and docs for agent messaging interface

--- uagents_utils.py
+++ uagents_utils.py
@@ -8,3 +8,44 @@ def mark_user_as_ordered(ctx: Context, user_id: str) -> None:
 
 def user_has_ordered(ctx: Context, user_id: str) -> bool: 
     return ctx.storage.get(_get_user_orderd_key(user_id))
+
+# TODO
+def get_message_history(chat_id: str) -> list:
+    """
+    TODO: Retrieve the message history for a particular chat ID.
+    
+    This function is a placeholder and needs to be implemented. We don't know how
+    the developer API will work yet. Developers should look into the implementation
+    and get back to the team with their findings.
+    
+    Args:
+        chat_id: The unique identifier for the chat whose message history should be retrieved
+    
+    Returns:
+        List of messages for the specified chat ID
+    
+    Note:
+        This is a TODO item. Implementation details need to be researched and discussed with the team.
+    """
+    pass
+
+# TODO
+def append_message_to_history(chat_id: str, message: str) -> None:
+    """
+    TODO: Append a message to the message history for a particular chat ID.
+    
+    This function is a placeholder and needs to be implemented. We don't know how
+    the developer API will work yet. Developers should look into the implementation
+    and get back to the team with their findings.
+    
+    Args:
+        chat_id: The unique identifier for the chat to append the message to
+        message: The message content to append to the chat history
+    
+    Returns:
+        None
+    
+    Note:
+        This is a TODO item. Implementation details need to be researched and discussed with the team.
+    """
+    pass
